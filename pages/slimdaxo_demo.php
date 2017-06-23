3d79ea42398bdec2abd2f02db601718f9a25f2b1
myCodebox <myCodebox@users.noreply.github.com>
2017-06-24 00:22:58 +0200

    Get token and header auth work now

--- pages/slimdaxo_demo.php
+++ pages/slimdaxo_demo.php
@@ -20,6 +20,10 @@
 		<div class="col-xs-12 col-sm-3">
 			<button type="button" class="btn btn-block btn-primary" id="gettoken">Get the Token</button>
 		</div>
+		<div class="col-xs-12"><hr /></div>
+		<div class="col-xs-12 col-sm-3 col-sm-offset-9">
+			<button type="button" class="btn btn-block btn-primary" id="testtoken">Test the Token</button>
+		</div>
 	</div>';
 
 	$fragment = new rex_fragment();
@@ -30,15 +34,23 @@
 ?>
 
 <script type="text/javascript">
-
 	(function () {
 		"use strict";
 
 		var App = App || {};
 
 		App.init = function () {
-			this.token = rex.slimdaxo_token; // b64utos(App.token)
+			this.token = rex.slimdaxo_path; // b64utos(App.token)
 			this.hash  = rex.slimdaxo_hash;
+		}
+
+		App.decodeToken = function(jwt){
+			var a = jwt.split(".");
+			return  b64utos(a[1]);
+		}
+		App.setJwt = function(token){
+			this.jwt = token;
+			// this.claim = this.decodeToken(token);
 		}
 
 		var gettoken = document.getElementById('gettoken');
@@ -49,10 +61,11 @@
 
 		App.gettoken = function () {
 			reqwest({
-				url: b64utos(App.token),
+				url: b64utos(App.token)+'/token',
 				method: 'post',
 				data: { hash: App.hash },
 				success: function (res) {
+					App.setJwt(res.token);
 					$('#token').text(res.token);
 					$('#expires').text(res.expires);
 					$('#hash').text(b64utos(res.hash));
@@ -60,14 +73,35 @@
 			});
 		};
 
+		var testtoken = document.getElementById('testtoken');
+		testtoken.addEventListener('click', function(e){
+			e.preventDefault();
+			App.testtoken();
+		}, false);
+
+		App.testtoken = function () {
+			reqwest({
+				url: b64utos(App.token)+'/test',
+				method: 'post',
+				contentType: 'application/json',
+				headers: {'Authorization':'Bearer '+App.jwt},
+				success: function (res) {
+					// $('.data_code').text(res.code);
+					console.log('OK');
+				},
+				error: function (err) {
+					// $('.data_code').text(err.status+' '+err.statusText);
+					// $('.data').text('');
+					console.log('NOK');
+				}
+			});
+		};
 
 		if (
-			(typeof rex.slimdaxo_token !== "undefined" && rex.slimdaxo_token != "")
+			(typeof rex.slimdaxo_path !== "undefined" && rex.slimdaxo_path != "")
 			&& (typeof rex.slimdaxo_hash !== "undefined" && rex.slimdaxo_hash != "")
 		) {
 			App.init();
 		}
 	})();
-
-
 </script>
